9ae4554f856ec7fd69e317e08c8b69223b97fddd
Drew Bisset <andrew.bisset@gmail.com>
2014-09-21 17:09:37 -0400

    Improve build process
    * Use symlinks for dotfiles
    * Provide support to overwrite/stash existing dotfiles
    * Install includes vimbundle setup

--- bin/vimbundles.sh
+++ bin/vimbundles.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-mkdir -p ~/.vimbundles
-cd ~/.vimbundles
+source 'bin/lib/tools'
 
 get_bundle() {
   (
@@ -14,20 +13,41 @@ get_bundle() {
   )
 }
 
+# prepare build directory
+echo $PWD
+source_path="$PWD/.vimbundles"
+
+mkdir $source_path
+
+# create symlinks
+link $source_path "$HOME/.vimbundles"
+
+cd $source_path
+
 # Tab completion
 get_bundle ervandew supertab
+
 # Delimeter defined tabular alignment
 get_bundle godlygeek tabular
+
 # Language Tools
+# Ruby
+get_bundle vim-ruby vim-ruby
+get_bundle nelstrom vim-textobj-rubyblock
+get_bundle tpope vim-rails
+get_bundle tpope vim-cucumber
+get_bundle tpope vim-bundler
+
+# JavaScript/CoffeeScript/Clientside
 get_bundle kchmck vim-coffee-script
 get_bundle leshill vim-json
 get_bundle pangloss vim-javascript
+get_bundle mustache vim-mustache-handlebars
+
 # Utils
 get_bundle mileszs ack.vim
 get_bundle tpope vim-abolish
-get_bundle tpope vim-bundler
 get_bundle tpope vim-commentary
-get_bundle tpope vim-cucumber
 get_bundle tpope vim-endwise
 get_bundle tpope vim-eunuch
 get_bundle tpope vim-fugitive
@@ -37,7 +57,6 @@ get_bundle tpope vim-markdown
 get_bundle tpope vim-pathogen
 get_bundle tpope vim-rake
 get_bundle tpope vim-ragtag
-get_bundle tpope vim-rails
 get_bundle tpope vim-repeat
 get_bundle tpope vim-rsi
 get_bundle tpope vim-sensible
@@ -45,7 +64,6 @@ get_bundle tpope vim-speeddating
 get_bundle tpope vim-surround
 get_bundle tpope vim-unimpaired
 get_bundle tpope vim-vividchalk
-get_bundle vim-ruby vim-ruby
 get_bundle vim-scripts bufkill.vim
 get_bundle vim-scripts bufexplorer.zip
 get_bundle jgdavey vim-blockle
@@ -63,12 +81,10 @@ get_bundle scrooloose syntastic
 get_bundle vim-scripts IndexedSearch
 get_bundle goldfeld vim-seek
 get_bundle kana vim-textobj-user
-get_bundle nelstrom vim-textobj-rubyblock
 get_bundle Rykka lastbuf.vim
 get_bundle altercation vim-colors-solarized
 get_bundle kien ctrlp.vim
 get_bundle scrooloose nerdtree
 get_bundle bling vim-airline
-get_bundle mustache vim-mustache-handlebars
 
 vim -c 'call pathogen#helptags()|q'
