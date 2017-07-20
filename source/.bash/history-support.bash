ab7846f0f328b56b087c26e93ddc80d3f6606734
Drew Bisset <andrew.bisset@gmail.com>
2017-07-20 17:25:02 -0400

    Use a more sensible histignore

--- source/.bash/history-support.bash
+++ source/.bash/history-support.bash
@@ -7,6 +7,6 @@ shopt -s histappend
 # immediately record history
 PROMPT_COMMAND='history -a'
 
-export HISTIGNORE='cd*:ls*:ll*:lla*:mv*:rm*:touch*:fg::history:man*:which*:cat*:less*:git push*:ps*:bundle exec rails s:bundle exec rails c:bundle install*:bundle exec rake db:migrate:npm install*:brew install*:rbenv install*:kill*:exit:vim*:clear:irb:node:nix-env --install*'
+export HISTIGNORE='cd*:ls*:ll*:lla*:mv*:rm*:touch*:history:man*:which*:cat*:less*:ps*:bundle install*:npm install*:brew install*:rbenv install*:exit:clear:irb:nix-env --install*'
 
 export HISTCONTROL=ignorespace:ignoredups
